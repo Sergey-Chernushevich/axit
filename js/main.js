e966680c54ceb063f64cd34b13971a67d3dbfb29
Sergey-Chernushevich <sergeychernushevich@gmail.com>
2022-07-22 22:57:46 +0300

    fix: fix styles and js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,5 @@
 const tabs = document.querySelectorAll(".tab-btn");
-console.log(tabs);
 const tabContents = document.querySelectorAll(".tab-info");
-console.log(tabContents);
 tabs.forEach((tab, index) => {
   tab.addEventListener("click", () => {
     tabContents.forEach((content) => {
